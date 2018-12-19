a3c04c07176bb6eba7336c813d92a763fb7ccb09
Alex Tan <AT212355@gmail.com>
2018-12-19 10:50:49 -0500

    Introduction

---
+++ javascript/calc.js
@@ -0,0 +1,61 @@
+let str = "";
+
+function button0() {
+  let p = document.getElementById("display");
+  str += "0";
+  p.innerHTML = str;
+}
+
+function button1() {
+  let p = document.getElementById("display");
+  str += "1";
+  p.innerHTML = str;
+}
+
+function button2() {
+  let p = document.getElementById("display");
+  str += "2";
+  p.innerHTML = str;
+}
+
+function button3() {
+  let p = document.getElementById("display");
+  str += "3";
+  p.innerHTML = str;
+}
+
+function button4() {
+  let p = document.getElementById("display");
+  str += "4";
+  p.innerHTML = str;
+}
+
+function button5() {
+  let p = document.getElementById("display");
+  str += "5";
+  p.innerHTML = str;
+}
+
+function button6() {
+  let p = document.getElementById("display");
+  str += "6";
+  p.innerHTML = str;
+}
+
+function button7() {
+  let p = document.getElementById("display");
+  str += "7";
+  p.innerHTML = str;
+}
+
+function button8() {
+  let p = document.getElementById("display");
+  str += "8";
+  p.innerHTML = str;
+}
+
+function button9() {
+  let p = document.getElementById("display");
+  str += "9";
+  p.innerHTML = str;
+}
