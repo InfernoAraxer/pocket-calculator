03a6b52d2e5eb39e37c63695d576ec68e617eac9
Alex Tan <AT212355@gmail.com>
2018-12-22 13:53:54 -0500

    Decoreated the Calc and added most of the Functions

--- javascript/calc.js
+++ javascript/calc.js
@@ -6,63 +6,95 @@ let factor1 = 1;
 let factor2 = 0;
 let counter = 1;
 
+function convert() {
+  if (product > 1) {
+    product = product/(10 ** (product.length - 1));
+    product = product.toString();
+    let parts = product.split(".");
+    console.log(parts[1], parts[0]);
+    parts[1] = parts[1]/(10 ** (parts[1].length));
+    let x = Number(parts[1]);
+    parts[1] = x.toFixed(5);
+    console.log(parts[1]);
+    product = parts[0] + parts[1] + "e+" + (product.length - 1);
+    console.log(product);
+    product = Number(product);
+  } else {
+    product = product*(10 ** (product.length + 1));
+    let parts = []
+    parts = product.slice(".");
+    parts[1] = parts[1].toFixed(5);
+    product = parts[0] + "." + parts[2] + "e-" + (product.length + 1);
+  }
+}
+
 function button0() {
   let p = document.getElementById("display");
   str += "0";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button1() {
   let p = document.getElementById("display");
   str += "1";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button2() {
   let p = document.getElementById("display");
   str += "2";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button3() {
   let p = document.getElementById("display");
   str += "3";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button4() {
   let p = document.getElementById("display");
   str += "4";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button5() {
   let p = document.getElementById("display");
   str += "5";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button6() {
   let p = document.getElementById("display");
   str += "6";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button7() {
   let p = document.getElementById("display");
   str += "7";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button8() {
   let p = document.getElementById("display");
   str += "8";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
 function button9() {
   let p = document.getElementById("display");
   str += "9";
+  if(str.length > 10) str = str.substring(0,10);
   p.innerHTML = str;
 }
 
@@ -84,10 +116,16 @@ function subtract() {
 
 function multiply() {
   let p = document.getElementById("display");
-    factor1 = Number(str);
-    p.innerHTML = str;
-    str = "";
-    i = 3;
+  factor1 = Number(str);
+  p.innerHTML = str;
+  str = "";
+  i = 3;
+}
+
+function decimal() {
+  let p = document.getElementById("display");
+  str += ".";
+  p.innerHTML = str;
 }
 
 function divide() {
@@ -97,13 +135,14 @@ function divide() {
   str = "";
   i = 4;
 }
-
+// fix
 function equal() {
   let p = document.getElementById("display");
   switch (i) {
     case 1:
       factor2 = Number(str);
       product = (factor1 + factor2);
+      product = product.toString();
       p.innerHTML = product;
       factor1 = product;
       str = "";
@@ -111,18 +150,21 @@ function equal() {
     case 2:
       factor2 = Number(str);
       product = (factor1 - factor2);
+      product = product.toString();
       p.innerHTML = product;
       str = "";
       break;
     case 3:
       factor2 = Number(str);
       product = (factor1 * factor2);
+      product = product.toString();
       p.innerHTML = product;
       str = "";
       break;
     case 4:
       factor2 = Number(str);
       product = (factor1 / factor2);
+      product = product.toString();
       p.innerHTML = product;
       str = "";
       break;
@@ -131,7 +173,7 @@ function equal() {
       break;
   }
 }
-
+//fix
 function allClear() {
   let p = document.getElementById("display");
   let str = 0;
@@ -142,21 +184,15 @@ function allClear() {
   let factor1 = 1;
   let factor2 = 0;
   let counter = 1;
-  str = "";
+  str = 0;
 }
-
+//fix
 function positiveAndNegative() {
   let p = document.getElementById("display");
   str = (str*(-1));
   p.innerHTML = str;
 }
-
-function decimal() {
-  let p = document.getElementById("display");
-  str += ".";
-  p.innerHTML = str;
-}
-
+//fix
 function percent() {
   let p = document.getElementById("display");
   if (factor1 == 1) {
@@ -198,3 +234,18 @@ function percent() {
     }
   }
 }
+
+//Need to Have or fix
+//Commas
+//exponential or scientific notation
+//Error when clicking one of the operations first
+//chained operations
+//maybe positive and Negative
+//All Clear
+/*
+Powers x2, x3, and xy)
+Roots (√x, 3√x, and y√x)
+Factorial (x!)
+Constants (π)
+Trigonometry (sin, cos, and tan)
+*/
