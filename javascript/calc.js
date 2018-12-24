d961670a8df4394d9d45a0908f3d2f46ef9f7ed7
Alex Tan <AT212355@gmail.com>
2018-12-24 14:56:45 -0500

    Added a few more features, random adding 1 error

--- javascript/calc.js
+++ javascript/calc.js
@@ -1,13 +1,15 @@
 let str = "";
 let i = 0;
 let j = 1;
-let product = 0;
-let factor1 = 1;
-let factor2 = 0;
-let counter = 1;
+let product = "";
+let factor1 = "";
+let factor2 = "";
+let v = 0;
 let x = 1;
 let y = 0;
-let z = 1;
+let r = 1;
+let w = 0;
+
 
 function convert() {
   if (product > 1) {
@@ -32,100 +34,296 @@ function convert() {
 }
 
 function button0() {
-  let p = document.getElementById("display");
-  str += "0";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += 0;
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    v++;
+  } else if (w > 0) {
+    let p = document.getElementById("display");
+    str += "0";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button1() {
-  let p = document.getElementById("display");
-  str += "1";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "1";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "1";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button2() {
-  let p = document.getElementById("display");
-  str += "2";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "2";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "2";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button3() {
-  let p = document.getElementById("display");
-  str += "3";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "3";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "3";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button4() {
-  let p = document.getElementById("display");
-  str += "4";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "4";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "4";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button5() {
-  let p = document.getElementById("display");
-  str += "5";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "5";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "5";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button6() {
-  let p = document.getElementById("display");
-  str += "6";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "6";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "6";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button7() {
-  let p = document.getElementById("display");
-  str += "7";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "7";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "7";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button8() {
-  let p = document.getElementById("display");
-  str += "8";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = 0;
+    let factor1 = 1;
+    let factor2 = 0;
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "8";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "8";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function button9() {
-  let p = document.getElementById("display");
-  str += "9";
-  if(str.length > 10) str = str.substring(0,10);
-  p.innerHTML = str;
+  if (v == 0) {
+    let i = 0;
+    let j = 1;
+    let product = "";
+    let factor1 = "";
+    let factor2 = "";
+    let x = 1;
+    let y = 0;
+    let r = 1;
+    let w = 0;
+    let p = document.getElementById("display");
+    str += "9";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+    v++;
+  } else {
+    let p = document.getElementById("display");
+    str += "9";
+    if(str.length > 10) str = str.substring(0,10);
+    p.innerHTML = str;
+    w++;
+  }
 }
 
 function add() {
-  if (z < 2) {
+  if (r < 2) {
+    console.log(factor1, str);
     let p = document.getElementById("display");
     factor1 = Number(str);
+    console.log(str);
     p.innerHTML = str;
     str = "";
-    z++;
+    r++;
+    console.log(r);
     x = 1;
+    v = 1;
   } else {
     let p = document.getElementById("display");
     if (x < 2) equal();
     p.innerHTML = product;
     str = "";
     x = 1;
+    v = 1;
   }
   i = 1;
 }
 
 function subtract() {
-  if (z < 2) {
+  if (r < 2) {
     let p = document.getElementById("display");
     factor1 = Number(str);
     p.innerHTML = str;
     str = "";
-    z++;
+    r++;
+    console.log(x , r);
+    v = 1;
     x = 1;
   } else {
     let p = document.getElementById("display");
@@ -133,44 +331,52 @@ function subtract() {
     p.innerHTML = product;
     str = "";
     x = 1;
+    v = 1;
   }
   i = 2;
 }
 
 function multiply() {
 
-  if (z < 2) {
+  if (r < 2) {
     let p = document.getElementById("display");
     factor1 = Number(str);
+    console.log(str);
     p.innerHTML = str;
     str = "";
-    z++;
     x = 1;
+    v = 1;
+    r++;
+    console.log(x , r);
   } else {
+    console.log(x, r, product, str);
     let p = document.getElementById("display");
     if (x < 2) equal();
     p.innerHTML = product;
     str = "";
     x = 1;
+    v = 1;
   }
   i = 3;
 }
 
 function divide() {
-
-  if (z < 2) {
+  if (r < 2) {
     let p = document.getElementById("display");
     factor1 = Number(str);
     p.innerHTML = str;
     str = "";
-    z++;
     x = 1;
+    v = 1;
+    r++;
+    console.log(r);
   } else {
     let p = document.getElementById("display");
     if (x < 2) equal();
     p.innerHTML = product;
     str = "";
     x = 1;
+    v = 1;
   }
   i = 4;
 }
@@ -184,7 +390,7 @@ function decimal() {
   }
 }
 
-// fix
+// fix?
 function equal() {
   let p = document.getElementById("display");
   switch (i) {
@@ -198,6 +404,7 @@ function equal() {
         factor1 = product;
         str = "";
         x++;
+        v = 0;
       } else {
         product = (factor1 + factor2);
         product = product.toString();
@@ -205,6 +412,7 @@ function equal() {
         product = Number(product);
         factor1 = product;
         str = "";
+        v = 0;
         x++;
       }
       break;
@@ -217,6 +425,7 @@ function equal() {
         product = Number(product);
         factor1 = product;
         str = "";
+        v = 0;
         x++;
       } else {
         product = (factor1 - factor2);
@@ -225,67 +434,70 @@ function equal() {
         product = Number(product);
         factor1 = product;
         str = "";
+        v = 0;
         x++;
       }
       break;
     case 3:
-    if (x < 2) {
-      factor2 = Number(str);
-      product = (factor1 * factor2);
-      product = product.toString();
-      p.innerHTML = product;
-      product = Number(product);
-      factor1 = product;
-      str = "";
-      x++;
-    } else {
-      product = (factor1 * factor2);
-      product = product.toString();
-      p.innerHTML = product;
-      product = Number(product);
-      factor1 = product;
-      str = "";
-      x++;
-    }
+      if (x < 2) {
+        factor2 = Number(str);
+        product = (factor1 * factor2);
+        product = product.toString();
+        p.innerHTML = product;
+        product = Number(product);
+        factor1 = product;
+        str = "";
+        v = 0;
+        x++;
+      } else {
+        product = (factor1 * factor2);
+        product = product.toString();
+        p.innerHTML = product;
+        product = Number(product);
+        factor1 = product;
+        str = "";
+        v = 0;
+        x++;
+      }
       break;
     case 4:
-    if (x < 2) {
-      factor2 = Number(str);
-      product = (factor1 / factor2);
-      product = product.toString();
-      p.innerHTML = product;
-      product = Number(product);
-      factor1 = product;
-      str = "";
-      x++;
-    } else {
-      product = (factor1 / factor2);
-      product = product.toString();
-      p.innerHTML = product;
-      product = Number(product);
-      factor1 = product;
-      str = "";
-      x++;
-    }
+      if (x < 2) {
+        factor2 = Number(str);
+        product = (factor1 / factor2);
+        product = product.toString();
+        p.innerHTML = product;
+        product = Number(product);
+        factor1 = product;
+        str = "";
+        v = 0;
+        x++;
+      } else {
+        product = (factor1 / factor2);
+        product = product.toString();
+        p.innerHTML = product;
+        product = Number(product);
+        factor1 = product;
+        str = "";
+        v = 0;
+        x++;
+      }
       break;
     default:
       p.innerHTML = str;
       break;
   }
 }
-//fix
+
 function allClear() {
   let p = document.getElementById("display");
+  let product = "";
+  let factor1 = "";
+  let factor2 = "";
   let str = 0;
   p.innerHTML = str;
-  let i = 0;
-  let j = 1;
-  let product = 0;
-  let factor1 = 1;
-  let factor2 = 0;
-  let counter = 1;
-  str = 0;
+  str = "";
 }
+
 //fix
 function positiveAndNegative() {
   let p = document.getElementById("display");
@@ -295,54 +507,24 @@ function positiveAndNegative() {
 //fix
 function percent() {
   let p = document.getElementById("display");
-  if (factor1 == 1) {
+  if (factor1 == 0) {
     str = (Number(str)/100); //
     p.innerHTML = str;
     str = "";
   } else {
     percent = (Number(str)/100); //.3
     str = (factor1 * percent) //30
-    switch (i) {
-      case 1:
-        factor2 = str;
-        product = (factor1 + factor2);
-        p.innerHTML = product;
-        factor1 = product;
-        str = "";
-        break;
-      case 2:
-        factor2 = str; //30
-        product = (factor1 - factor2);
-        p.innerHTML = product;
-        str = "";
-        break;
-      case 3:
-        factor2 = str;
-        product = (factor1 * factor2);
-        p.innerHTML = product;
-        str = "";
-        break;
-      case 4:
-        factor2 = str;
-        product = (factor1 / factor2);
-        p.innerHTML = product;
-        str = "";
-        break;
-      default:
-        p.innerHTML = str;
-        break;
-    }
+    equal();
   }
 }
 
 //Need to Have or fix
+
 //Commas
 //Exponential or scientific notation
 //Error when clicking one of the operations first and in general
-//Fix 0 spam
 //Chained operations - Test it more later
-//Maybe positive and Negative
-//All Clear
+//Maybe positive and Negative with product
 /*
 Powers x2, x3, and xy)
 Roots (√x, 3√x, and y√x)
